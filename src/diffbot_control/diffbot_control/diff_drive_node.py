1a7bc38b45915f5b4c69e1df823b5f9352a6720c
TRAN-RUESCHE Bastien <Bastien.Tran-Ruesche@bordeaux-inp.fr>
2026-02-04 14:23:36 +0100

    nothing changed

--- src/diffbot_control/diffbot_control/diff_drive_node.py
+++ src/diffbot_control/diffbot_control/diff_drive_node.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
-from diffbot_control.esc_driver import ESCDriver
+from diffbot_control.diffbot_control.esp_driver import ESPDriver
 
 
 class DiffDriveNode(Node):
@@ -13,19 +13,15 @@ class DiffDriveNode(Node):
         # Parameters
         self.declare_parameter('wheel_base', 0.30)  # distance entre roues (m)
         self.declare_parameter('use_sim', True)
-        self.declare_parameter('left_esc_port', '/dev/ttyUSB0')
-        self.declare_parameter('right_esc_port', '/dev/ttyUSB1')
+        self.declare_parameter('esp_port', '/dev/ttyUSB0')
 
         self.L = self.get_parameter('wheel_base').value
         self.use_sim = self.get_parameter('use_sim').value
 
         # ESC drivers (mode réel)
         if not self.use_sim:
-            self.left_esc = ESCDriver(
-                self.get_parameter('left_esc_port').value
-            )
-            self.right_esc = ESCDriver(
-                self.get_parameter('right_esc_port').value
+            self.ucontroller = ESPDriver(
+                port=self.get_parameter('esp_port').value
             )
 
         # Subscriber
@@ -50,16 +46,15 @@ class DiffDriveNode(Node):
         if self.use_sim:
             self.simulate(v_left, v_right)
         else:
-            self.send_to_esc(v_left, v_right)
+            self.send_to_esp(v_left, v_right)
 
     def simulate(self, v_left, v_right):
         self.get_logger().info(
             f"[SIM] left={v_left:.2f} m/s | right={v_right:.2f} m/s"
         )
 
-    def send_to_esc(self, v_left, v_right):
-        self.left_esc.set_speed(v_left)
-        self.right_esc.set_speed(v_right)
+    def send_to_esp(self, v_left, v_right):     
+        self.ucontroller.set_speeds(v_left, v_right)
 
 
 def main():
