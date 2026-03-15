e5f8d7bb2516e9455373d36457aed8a395f294a2
TRAN-RUESCHE Bastien <Bastien.Tran-Ruesche@bordeaux-inp.fr>
2026-03-15 21:01:46 +0100

    change to micro-ros

--- src/diffdrive_arduino-humble/hardware/include/diffdrive_arduino/diffbot_system.hpp
+++ src/diffdrive_arduino-humble/hardware/include/diffdrive_arduino/diffbot_system.hpp
@@ -1,106 +1,36 @@
-// Copyright 2021 ros2_control Development Team
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#ifndef DIFFBOT_MICRO_ROS_HPP
+#define DIFFBOT_MICRO_ROS_HPP
 
-#ifndef DIFFDRIVE_ARDUINO__DIFFBOT_SYSTEM_HPP_
-#define DIFFDRIVE_ARDUINO__DIFFBOT_SYSTEM_HPP_
-
-#include <memory>
-#include <string>
-#include <vector>
-
-#include "hardware_interface/handle.hpp"
-#include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
-#include "hardware_interface/types/hardware_interface_return_values.hpp"
-#include "rclcpp/clock.hpp"
-#include "rclcpp/duration.hpp"
-#include "rclcpp/macros.hpp"
-#include "rclcpp/time.hpp"
-#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
-#include "rclcpp_lifecycle/state.hpp"
-#include "diffdrive_arduino/visibility_control.h"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 
-#include "diffdrive_arduino/arduino_comms.hpp"
-#include "diffdrive_arduino/wheel.hpp"
-
-namespace diffdrive_arduino
+namespace diffbot_micro_ros
 {
-class DiffDriveArduinoHardware : public hardware_interface::SystemInterface
+using hardware_interface::CallbackReturn;
+using hardware_interface::return_type;
+
+class DiffBotMicroRos : public hardware_interface::SystemInterface
 {
-
-struct Config
-{
-  std::string left_wheel_name = "";
-  std::string right_wheel_name = "";
-  float loop_rate = 0.0;
-  std::string device = "";
-  int baud_rate = 0;
-  int timeout_ms = 0;
-  int enc_counts_per_rev = 0;
-  int pid_p = 0;
-  int pid_d = 0;
-  int pid_i = 0;
-  int pid_o = 0;
-};
-
-
 public:
-  RCLCPP_SHARED_PTR_DEFINITIONS(DiffDriveArduinoHardware);
-
-  DIFFDRIVE_ARDUINO_PUBLIC
-  hardware_interface::CallbackReturn on_init(
-    const hardware_interface::HardwareInfo & info) override;
-
-  DIFFDRIVE_ARDUINO_PUBLIC
+  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-
-  DIFFDRIVE_ARDUINO_PUBLIC
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
-
-  DIFFDRIVE_ARDUINO_PUBLIC
-  hardware_interface::CallbackReturn on_configure(
-    const rclcpp_lifecycle::State & previous_state) override;
-
-  DIFFDRIVE_ARDUINO_PUBLIC
-  hardware_interface::CallbackReturn on_cleanup(
-    const rclcpp_lifecycle::State & previous_state) override;
-
-
-  DIFFDRIVE_ARDUINO_PUBLIC
-  hardware_interface::CallbackReturn on_activate(
-    const rclcpp_lifecycle::State & previous_state) override;
-
-  DIFFDRIVE_ARDUINO_PUBLIC
-  hardware_interface::CallbackReturn on_deactivate(
-    const rclcpp_lifecycle::State & previous_state) override;
-
-  DIFFDRIVE_ARDUINO_PUBLIC
-  hardware_interface::return_type read(
-    const rclcpp::Time & time, const rclcpp::Duration & period) override;
-
-  DIFFDRIVE_ARDUINO_PUBLIC
-  hardware_interface::return_type write(
-    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+  return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+  return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
+  // micro-ROS Communication
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr cmd_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr state_sub_;
 
-  ArduinoComms comms_;
-  Config cfg_;
-  Wheel wheel_l_;
-  Wheel wheel_r_;
+  // Memory buffers for the Controller Manager (Pointers)
+  // Indices: 0 = Left, 1 = Right
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_positions_;
+  std::vector<double> hw_velocities_;
 };
-
-}  // namespace diffdrive_arduino
-
-#endif  // DIFFDRIVE_ARDUINO__DIFFBOT_SYSTEM_HPP_
+} 
+#endif
