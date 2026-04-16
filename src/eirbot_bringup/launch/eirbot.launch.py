c53370cfd6806758dec271059199d235b51ea1f2
TRAN-RUESCHE Bastien <Bastien.Tran-Ruesche@bordeaux-inp.fr>
2026-04-16 22:16:29 +0200

    add tf for later

--- src/eirbot_bringup/launch/eirbot.launch.py
+++ src/eirbot_bringup/launch/eirbot.launch.py
@@ -53,4 +53,11 @@ def generate_launch_description():
 
         # 5. Eirbot Mission Manager (Le cerveau)
         Node(package='eirbot_main', executable='mission_manager', output='screen'),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            # x y z yaw pitch roll frame_id child_frame_id
+            arguments=['0.1', '0', '0.2', '0', '0', '0', 'base_link', 'laser']
+        )
     ])
